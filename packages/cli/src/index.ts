ac116101d875470596aa15318ceab00886949698
YutaUra <yuuta3594@outlook.jp>
2021-04-09 22:32:36 +0900

    fix cli

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,13 +1,6 @@
-#!/usr/bin/env node
 export { base, condition, func, literal, op, stmt } from '@firestore-rules/core'
 export {
   FirestoreRulesModel,
   IFirestoreRulesModel,
   IFirestoreRulesModelConstructor,
 } from '@firestore-rules/model'
-import { cosmiconfigSync } from 'cosmiconfig'
-
-const explorerSync = cosmiconfigSync('firestore-rules')
-const searchedFor = explorerSync.search()
-
-console.log(searchedFor)
